cdf07e85c561d22416f06ea0c3fd0862182f7ef1
Jan Mayer <jan.mayer@ikp.uni-koeln.de>
2022-12-23 14:43:02 +0100

    Add doctests with coverage

--- application/ping/__init__.py
+++ application/ping/__init__.py
@@ -4,5 +4,11 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    """ doctest
+    >>> req = func.HttpRequest(method="get", url="ping", body=None)
+    >>> res = main(req)
+    >>> res.get_body().decode("utf-8")
+    'pong'
+    """
     logging.info("Python HTTP trigger function processed a request: %s", req.url)
     return func.HttpResponse("pong")
